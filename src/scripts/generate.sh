c34fe673ccadbf50e02f20e3c1f060a1c2296052
pavelvoit <paulo110895@gmail.com>
2020-08-19 00:35:46 +0300

    update logic

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -1,1 +1,11 @@
-envsubst '\$WEBHOOK_HOST' < /src/config/nginx/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
+
+export INCLUDE="include /etc/nginx/conf.d/certificate/certificate.conf*;"
+
+FILE=/var/www/certificate/certificate-cert.pem
+if [ -f "$FILE" ]; then
+    export INCLUDE="include /etc/nginx/conf.d/certificate/certificate.conf*;"
+else
+    export INCLUDE=""
+fi
+
+envsubst '\$WEBHOOK_HOST \$INCLUDE' < /src/config/nginx/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
