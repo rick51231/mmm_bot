67da10dc347a81dccacb0b898ce2c9d743b1b562
pavelvoit <paulo110895@gmail.com>
2020-08-19 00:03:38 +0300

    update template

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -1,2 +1,2 @@
-envsubst < /src/config/nginx/nginx.conf.template > /etc/nginx/conf.d/nginx.conf # /etc/nginx/conf.d/default.conf
+envsubst < /src/config/nginx/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
 nginx -g "daemon off;"
