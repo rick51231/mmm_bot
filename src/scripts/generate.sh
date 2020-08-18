db054a290470429fc9a15a83f6bf089467ddb582
pavelvoit <paulo110895@gmail.com>
2020-08-19 00:54:28 +0300

    update bash

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -2,11 +2,16 @@
 export INCLUDE="include /etc/nginx/conf.d/certificate/certificate.conf*;"
 
 FILE=/var/www/certificate/certificate-cert.pem
-if [ -f "$FILE" ]; then
-    export INCLUDE="include /etc/nginx/conf.d/certificate/certificate.conf*;"
-else
-    /code/certificate/acme.sh --issue --dns dns_cf -d bot."$WEBHOOK" -d www.""$WEBHOOK" --key-file /code/certificate/certificate-key.pem --fullchain-file /code/certificate/certificate-cert.pem
-    export INCLUDE=""
-fi
+
+for (( i=1; i <= 10; i++ ))
+do
+  if [ -f "$FILE" ]; then
+      export INCLUDE="include /etc/nginx/conf.d/certificate/certificate.conf*;"
+      break
+  else
+      /code/certificate/acme.sh --issue --dns dns_cf -d bot."$WEBHOOK" -d www."$WEBHOOK" --key-file /code/certificate/certificate-key.pem --fullchain-file /code/certificate/certificate-cert.pem
+  fi
+done
+
 
 envsubst '\$WEBHOOK_HOST \$INCLUDE' < /src/config/nginx/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
