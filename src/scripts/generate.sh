f1615e1b035d23ea2cdd98fd94017202bdad9f24
pavelvoit <paulo110895@gmail.com>
2020-08-19 11:58:02 +0300

    fix

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -1,6 +1,5 @@
 
 export INCLUDE="include /etc/nginx/conf.d/certificate/certificate.conf*;"
-
 FILE=/var/www/certificate/certificate-cert.pem
 
 for (( i=1; i <= 10; i++ ))
@@ -9,7 +8,7 @@ do
       export INCLUDE="include /etc/nginx/conf.d/certificate/certificate.conf*;"
       break
   else
-      /code/certificate/acme.sh --issue --dns dns_cf -d bot."$WEBHOOK" -d www."$WEBHOOK" --key-file /code/certificate/certificate-key.pem --fullchain-file /code/certificate/certificate-cert.pem
+      /code/certificate/acme.sh --issue --dns dns_cf -d $WEBHOOK_HOST -d www.$WEBHOOK_HOST --key-file /code/certificate/certificate-key.pem --fullchain-file /code/certificate/certificate-cert.pem
   fi
 done
 
