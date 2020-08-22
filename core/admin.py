4dd19b0a81bd0435ed60ea700dd7af93ef4cc10b
pavelvoit <paulo110895@gmail.com>
2020-08-22 16:01:15 +0300

    admin fields

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,7 @@ from core.models import Person, Settings, MessagePastFinish
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    fields = ("telegram_id", "username", "first_name", "last_name", "bot", "date_creation")
+    list_display = ("telegram_id", "username", "first_name", "last_name", "bot", "date_creation")
     list_filter = ("bot", "date_creation")
 
 
@@ -16,5 +16,5 @@ class SettingsAdmin(admin.ModelAdmin):
 
 @admin.register(MessagePastFinish)
 class MessagePastFinishAdmin(admin.ModelAdmin):
-    fields = ("days", "hours", "minute", "text", "video")
+    list_display = ("days", "hours", "minute", "text", "video")
 
